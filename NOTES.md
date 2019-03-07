6ead446b7ef4844fc1b99d78c7a28dcff006d6ea
bbennett7 <bryn.bennett@gmail.com>
2019-03-06 16:17:32 -0800

    Setting up the infrastructure for options for my CLI

--- NOTES.md
+++ NOTES.md
@@ -1,10 +1,10 @@
 
 
 How to build a CLI Gem
-1. Plan your gem, imagine your interface
-2. Start with the project structure - google
-3. Start with the entry point - the file run
-4. Force that to build the CLI
+  1. Plan your gem, imagine your interface
+  2. Start with the project structure - google
+  3. Start with the entry point - the file run
+  4. Force that to build the CLI
 5. Stub out the interface
 6. Start making things real
 7. Discover objects
@@ -22,12 +22,22 @@ Gives options to sort through a list of upcoming events.
 4. View events by time of day
 5. View all upcoming tree_people events
 
-Choose how to view, prompt to select an event for details.
+  Prompt user to select how to view
 
-Provides:
-1. Day of week, date, time
-2. Location
-3. Description
-5. Neighborhood
-6. Spaces available
-7. Where to sign up 
+Offers different options for each (list of location, list of type, etc.)
+
+  Prompts user to select a specific
+
+Shows all events that match that criteria
+1. Monday, March 11 at 11am - Moonlight Hike in Beverly Hills
+2. Monday March 11 at 2pm - Street Planting in Van Nuys
+
+  Prompt user to choose an event to get details on
+
+Shows for the chosen event:
+- Monday, March 11 from 11am to 2pm
+- Location: Beverly Hills
+- Description: Enjoy a moonlight hike in Beverly Hills
+- Availability: 6 spots open
+- To sign up for this event, please visit: <URL>
+- Type menu to return to the menu, or exit to exit the program.
--- lib/tree_people/cli.rb
+++ lib/tree_people/cli.rb
@@ -2,11 +2,101 @@
 
 class TreePeople::CLI
   def call
-    puts "Welcome to TreePeople!"
-    view_events
+    puts "Welcome to TreePeople! We are excited to have you join our team of volunteers."
+    menu
+
+    #exit
   end
 
-  def view_events
+  def menu
+    puts "Please select from the following list to view different TreePeople events, or type exit."
+    puts "   1. View by location"
+    puts "   2. View by event type"
+    puts "   3. View by day of the week"
+    puts "   4. View by time of day"
+    puts "   5. View all upcoming events" #possibly change this to month
+    user_selection = gets.chomp.downcase
+    while user_selection != "exit"
+      case user_selection
+      when "1" || "location"
+        location_options
+      when "2" || "event type"
+        event_type_options
+      when "3" || "day of the week"
+        day_of_week_options
+      when "4" || "time of day"
+        time_of_day_options
+      when "5" || "view all"
+        list_all
+      else
+        puts "We don't understand that command. Please select from the list of options, or type exit"
+        menu
+      end
+    end
+  end
 
+  def location_options
+    puts "We have upcoming events at the following locations:"
+    puts "1. Beverly Hills"
+    puts "2. Van Nuys"
+    puts "Which location would you like to see upcoming events for?"
+    location_choice = gets.chomp
+
+    if location_choice = "Beverly Hills"
+      puts "1. BevHills Event 1"
+      puts "2. BevHills Event 2"
+    elsif @location_choice = "Van Nuys"
+      puts "1. Van Nuys Event 1"
+      puts "2. Van Nuys Event 2"
+    else
+      puts "I'm sorry, we don't have any events in that location. Please try again."
+      location_options
+    end
+
+    puts "Which event would you like to see details for?"
+    event_choice = gets.chomp
+
+    while event_choice != "exit"
+      
+
+  end
+
+  def location
+  end
+
+  def event_type_options
+    puts "We have the following event types upcoming:"
+    puts "1. Hike"
+    puts "2. Cleanup"
+    puts "Which event type would you like to see upcoming events for?"
+    @event_type_choice = gets.chomp
+  end
+
+  def day_of_week_options
+    puts "Which day would you like to see upcoming events for?"
+    puts "Monday"
+    puts "Tuesday"
+    puts "Wednesday"
+    @day_choice = gets.chomp
+  end
+
+  def time_of_day_options
+    puts "Which time would you like to see upcoming events for?"
+    puts "Morning"
+    puts "Afternoon"
+    puts "Evening"
+    @time_choice = gets.chomp
+  end
+
+  def list_all
+    puts "Here is a list of all upcoming TreePeople events:"
+    puts "1. Event1"
+    puts "2. Event2"
+    puts "Which event would you like to see details for?"
+    @event_choice = gets.chomp
+  end
+
+  def exit
+    puts "Thank you for considering TreePeople. We hope to see you at an event soon!"
   end
 end
